b0a5f5776ceb697b917ac62d6484712d00cda04b
Kennedy2042 <ogbonnakenedy@gmail.com>
2023-09-22 14:54:39 +0100

    first commit

--- script.js
+++ script.js
@@ -42,88 +42,31 @@ function startTimer() {
     }
     if (miliSecond > 99) {
         second++;
-        appendSec.innerHTML = second <= 9 ? "0" + second : second;
+        appendSec.innerHTML =  "0" + second;
         miliSecond = 0;
     }
-    // if (second > 9) {
-    //     appendSec.innerHTML = second;
-    // }
-    if(second > 59) {
-        second = 0;
-        minute++;
-        appendMin.innerHTML = minute <= 9 ? "0" + minute : minute ;
+    if (second > 9) {
+        appendSec.innerHTML = second;
     }
     else{
-        appendMin.innerHTML = minute <= 9 ? "0" + minute : minute;;
+        // appendSec.innerHTML = second
+    }
+    if(second > 59) {
+        minute++;
+        second = 0;
+        appendMin.innerHTML = "0" + minute;
+    }
+    else{
+        appendMin.innerHTML =  minute;
     }
     if(minute > 59){
         hour++;
         minute = 0;
-        appendHour.innerHTML = hour <= 9 ? "0" + hour : hour;
+        appendHour.innerHTML = "0" + hour;
     }
     else {
-        appendHour.innerHTML = hour <= 9 ? "0" + hour : hour;
+        appendHour.innerHTML = hour;
     }
 
 
 }
-
-
-
-// let unit = "00";
-// let tens = "00";
-// let hundred = "00";
-// let thousand = "00";
-// const hour = document.getElementById("hour");
-// const mins = document.getElementById("mins");
-// const seconds = document.getElementById("seconds");
-// const miliSecs = document.getElementById("miliSecs");
-// const startBtn = document.getElementById("startBtn");
-// const stopBtn = document.getElementById("stopBtn");
-// const resetBtn = document.getElementById("resetBtn");
-// let Interval;
-
-// startBtn.onclick = function () {
-//     clearInterval(Interval);
-//     Interval = setInterval(startTimer, 10);
-// }
-
-// stopBtn.onclick = function () {
-//     clearInterval(Interval)
-// }
-
-// resetBtn.onclick = function () {
-//     clearInterval(Interval);
-//     unit = "00"
-//     tens = "00";
-//     hundred = "00";
-//     thousand = "00";
-//     miliSecs.innerHTML = unit;
-//     seconds.innerHTML = tens;
-//     mins.innerHTML = hundred;
-//     hour.innerHTML = thousand;
-// }
-
-// function startTimer() {
-//     unit++;
-//     if(unit <= 9){
-//         miliSecs.innerHTML = "0" + unit;
-
-//     }
-//     if(unit > 9){
-//     miliSecs.innerHTML = unit;
-
-//     }
-
-//     if(unit > 99){
-//         tens++;
-//     seconds.innerHTML = "0" + tens;
-//     unit = 0;
-//     miliSecs.innerHTML = "0" + 0;
-//     }
-
-//     if (tens > 9){
-//     seconds.innerHTML = tens;
-
-//     }
-// }
